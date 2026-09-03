aa1f75dad91bef7bf6758337dedeba45d0220b44
juandn84-web <juandn84@gmail.com>
2026-09-04 01:16:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='gastos-v16';
+const C='gastos-v17';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./icon.png'])));
   self.skipWaiting();
@@ -7,10 +7,23 @@ self.addEventListener('activate',e=>{
   e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
+self.addEventListener('message',e=>{if(e.data==='skipWaiting')self.skipWaiting();});
 self.addEventListener('fetch',e=>{
+  const req=e.request;
+  const isDoc=req.mode==='navigate'||(req.destination==='document')||/\.html($|\?)/.test(req.url);
+  if(isDoc){
+    // RED PRIMERO para el HTML: así una versión nueva entra siempre que haya conexión.
+    e.respondWith(
+      fetch(req,{cache:'no-store'}).then(res=>{
+        const cp=res.clone();caches.open(C).then(c=>c.put(req,cp));
+        return res;
+      }).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html')))
+    );
+    return;
+  }
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-      const cp=res.clone();caches.open(C).then(c=>c.put(e.request,cp));
+    caches.match(req).then(r=>r||fetch(req).then(res=>{
+      const cp=res.clone();caches.open(C).then(c=>c.put(req,cp));
       return res;
     }).catch(()=>caches.match('./index.html')))
   );
