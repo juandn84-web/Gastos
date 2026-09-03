9a89c0510379fd08fe9c60ec96472e84693e8a1f
juandn84-web <juandn84@gmail.com>
2026-09-04 01:30:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='gastos-v17';
+const C='gastos-v18';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./icon.png'])));
   self.skipWaiting();
