c19003be91e855f2ddc6a2e532e70e7db07de009
juandn84-web <juandn84@gmail.com>
2026-09-03 23:44:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='gastos-v12';
+const C='gastos-v13';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./icon.png','./xlsx.min.js'])));
   self.skipWaiting();
